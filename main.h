ac4537f29bc3b5102bc6fabcfd019fa86e89d340
root <root@DESKTOP-9EG7JEI.localdomain>
2023-07-17 16:01:31 +0300

    Header File

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <dirent.h>
+#endif
